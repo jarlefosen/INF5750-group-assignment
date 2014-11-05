0938ae59880170dc43318732396a8ad86d561584
amumei <amundmeisal@gmail.com>
2014-11-05 16:26:56 +0100

    Further styling on inboxView

--- www/js/controllers/indexCtrl.js
+++ www/js/controllers/indexCtrl.js
@@ -27,6 +27,30 @@ define([
             date: "2014-08-19T07:49:38.621+0000",
             unread: true,
             followUp: false
+          },
+          {
+            sender: "Kåre Conradi",
+            subject: "Takk for hjelpen!",
+            content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea eligendi incidunt maiores nesciunt obcaecati, quae. Eaque inventore recusandae sit sunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea eligendi incidunt maiores nesciunt obcaecati, quae. Eaque inventore recusandae sit sunt.",
+            date: "2014-08-19T07:49:38.621+0000",
+            unread: false,
+            followUp: false
+          },
+          {
+            sender: "Maria Magdalena Jucovich Menarita",
+            subject: "Business proposal with long subject line! Too long in fact",
+            content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea eligendi incidunt maiores nesciunt obcaecati, quae. Eaque inventore recusandae sit sunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea eligendi incidunt maiores nesciunt obcaecati, quae. Eaque inventore recusandae sit sunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea eligendi incidunt maiores nesciunt obcaecati, quae. Eaque inventore recusandae sit sunt.",
+            date: "2014-08-19T07:49:38.621+0000",
+            unread: false,
+            followUp: true
+          },
+          {
+            sender: "Jens Stoltenberg",
+            subject: "Diare på IFI",
+            content: "Utbruddet startet etter besk smak på kotelettene fra SIO kantina! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea eligendi incidunt maiores nesciunt obcaecati, quae. Eaque inventore recusandae sit sunt.",
+            date: "2014-08-19T07:49:38.621+0000",
+            unread: true,
+            followUp: false
           }
         ]
     }
