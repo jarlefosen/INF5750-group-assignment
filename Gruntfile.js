0a163a1d3c42daf8728fc1376f5843ffcd74e13f
Jarle Fosen <jarlefosen@gmail.com>
2014-11-05 15:42:12 +0100

    Testing eslinting commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function(grunt) {
-  "use strict";
+//  "use strict";
 
   grunt.initConfig({
     eslint: {
@@ -11,6 +11,6 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks("grunt-eslint");
 
 };
