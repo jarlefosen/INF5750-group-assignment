0c90e2a0720ec29fc00af1c4233ca32253c82b4b
Christian Bergum Bergersen <christian@bergersenweb.com>
2014-11-17 11:04:01 +0100

    Added newMessage() function

--- www/js/services/messageService.js
+++ www/js/services/messageService.js
@@ -11,16 +11,15 @@ define([
     function (ServerConfig, $q, $http) {
 
       var MESSAGES_BASE_URL = "/api/messageConversations";
-      var MY_INBOX_URL = "/api/me/inbox";
 
       function getAllMessages() {
         var deferred = $q.defer();
 
-        $http.get(ServerConfig.host + MY_INBOX_URL)
-          .success(function(data){
+        $http.get(ServerConfig.host + MESSAGES_BASE_URL)
+          .success(function (data) {
             deferred.resolve(data);
           })
-          .error(function(){
+          .error(function () {
             deferred.reject();
           });
 
@@ -31,10 +30,10 @@ define([
         var deferred = $q.defer();
 
         $http.get(ServerConfig.host + MESSAGES_BASE_URL + "/" + messageId)
-          .success(function(data){
+          .success(function (data) {
             deferred.resolve(data);
           })
-          .error(function(){
+          .error(function () {
             deferred.reject();
           });
         return deferred.promise;
@@ -44,11 +43,55 @@ define([
         var deferred = $q.defer();
 
         $http.delete(ServerConfig.host + MESSAGES_BASE_URL + "/" + messageId)
-          .success(function(data){
-            deferred.resolve(data);
+          .success(function (data) {
+            console.log("YEY!" + data);
           })
-          .error(function(){
-            deferred.reject();
+          .error(function () {
+
+          });
+        return deferred.promise;
+        //Messages.delete({messageId: messageId});
+      }
+
+      function newMessage(subject, text, user_receivers, group_receivers, org_receivers) {
+        var deferred = $q.defer();
+
+        var user_list = [];
+        angular.forEach(user_receivers, function (entry) {
+          var l = {id: entry};
+          user_list.push(l);
+        });
+
+        var group_list = [];
+        angular.forEach(group_receivers, function (entry) {
+          var l = {id: entry};
+          group_list.push(l);
+        });
+
+        var org_list = [];
+        angular.forEach(org_receivers, function (entry) {
+          var l = {id: entry};
+          org_receivers.push(l);
+        });
+
+        console.log("user_list = " + user_list);
+        console.log("group_list = " + group_list);
+        console.log("unit_list = " + org_list);
+
+        var message = {
+          subject: subject,
+          text: text,
+          users: user_list,
+          userGroups: group_list,
+          organisationUnits: org_list
+        };
+
+        $http.post(ServerConfig.host + MESSAGES_BASE_URL, message)
+          .success(function (data) {
+            console.log("POST message");
+          })
+          .error(function () {
+            console.log("Error POSTing:(");
           });
         return deferred.promise;
       }
@@ -56,7 +99,8 @@ define([
       return {
         getAllMessages: getAllMessages,
         getMessage: getMessage,
-        deleteMessage: deleteMessage
+        deleteMessage: deleteMessage,
+        newMessage: newMessage
       };
     }
   ]);
