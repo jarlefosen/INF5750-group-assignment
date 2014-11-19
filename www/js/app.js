f878babccfbb235b6d016e56eb134b8169c41762
amumei <amundmeisal@gmail.com>
2014-11-19 16:49:43 +0100

    Added ui-router as a dependency

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,8 @@
 define(
-  ["angular", "angular-route", "angular-resource"],
+  ["angular", "angular-route", "angular-resource", "angular-ui-router"],
   function (angular) {
     "use strict";
 
-    return angular.module("app", ["ngRoute", "ngResource"]);
+    return angular.module("app", ["ngRoute", "ngResource", "ui.router"]);
   }
 );
--- www/js/main.js
+++ www/js/main.js
@@ -2,12 +2,16 @@ require.config({
   paths: {
     "angular": "../bower_components/angular/angular",
     "angular-resource": "../bower_components/angular-resource/angular-resource",
-    "angular-route": "../bower_components/angular-route/angular-route"
+    "angular-route": "../bower_components/angular-route/angular-route",
+    "angular-ui-router": "../bower_components/angular-ui-router/release/angular-ui-router"
   },
   shim: {
     "angular": {exports: "angular"},
     "angular-route": ["angular"],
-    "angular-resource": ["angular"]
+    "angular-resource": ["angular"],
+    "angular-ui-router": {
+      deps: ['angular']
+    }
   }
 });
 
