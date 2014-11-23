7472a59fab432a936c556664d5896b5476f4c8b5
Jarle Fosen <jarlefosen@gmail.com>
2014-11-23 17:54:17 +0100

    Added W3C validation to Project and Grunt.
    run 'grunt html-validation' for html validation
    run 'grunt css-validation' for css validation
    run 'grunt validation' for both css and html validation

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,13 +20,46 @@ module.exports = function(grunt) {
         singleRun: true,
         configFile: "test/config/karma.conf.js"
       }
+    },
+    "html-validation": {
+      options: {
+        reset: grunt.option('reset') || true,
+        stoponerror: false,
+        relaxerror: [
+          "Bad value X-UA-Compatible for attribute http-equiv on element meta.",
+          "no document type declaration; will parse without validation"
+        ] //ignores these errors
+      },
+      files: {
+        src: [
+          'www/*.html',
+          'www/partials/*.html'
+        ]
+      }
+    },
+    "css-validation": {
+      options: {
+        reset: grunt.option('reset') || false,
+        stoponerror:false,
+        relaxerror: [],
+        profile: 'css3', // possible profiles are: none, css1, css2, css21, css3, svg, svgbasic, svgtiny, mobile, atsc-tv, tv
+        medium: 'all', // possible media are: all, aural, braille, embossed, handheld, print, projection, screen, tty, tv, presentation
+        warnings: '0' // possible warnings are: 2 (all), 1 (normal), 0 (most important), no (no warnings)
+      },
+      files: {
+        src: ['www/css/*.css']
+      }
     }
   });
 
   grunt.loadNpmTasks("grunt-eslint");
   grunt.loadNpmTasks("grunt-karma");
+  grunt.loadNpmTasks('grunt-w3c-validation');
 
   grunt.registerTask("test", ["karma:test"]);
   grunt.registerTask("test:fast", ["karma:fast"]);
+  grunt.registerTask("validation", ["html-validation","css-validation"]);
+  //grunt.registerTask("validate-html", ["html-validation"]);
+  //grunt.registerTask("validate-css", ["css-validation"]);
 
 };
