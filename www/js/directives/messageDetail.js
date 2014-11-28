471207a441c054ec6e071c6b00c7fe496abc0f7b
Jarle Fosen <jarlefosen@gmail.com>
2014-11-28 15:32:58 +0100

    Directive now calls MessageService instead of scope functions

--- www/js/directives/messageDetail.js
+++ www/js/directives/messageDetail.js
@@ -2,39 +2,44 @@ define([
   "app"
 ], function(app){
   "use strict";
-  app.directive("messageDetail", function(){
-    return {
-      restrict: "E",
-      replace: true,
-      templateUrl: "partials/singleMessage.html",
-      scope: {
-        message: "=message",
-        messageid: "=messageid",
-        setFollowUp: "@",
-        replyFn: "&"
-      },
-      link: function(scope){
-        scope.content = "";
-        /* Evaluate scope reply function */
-        var reply = scope.replyFn();
+  app.directive("messageDetail",
+    ["MessageService", "$state",
+      function(MessageService, $state){
+        return {
+          restrict: "E",
+          replace: true,
+          templateUrl: "partials/singleMessage.html",
+          scope: {
+            message: "=message",
+            messageid: "=messageid",
+            setFollowUp: "@",
+            replyFn: "&"
+          },
+          link: function(scope){
+            scope.content = "";
+            /* Evaluate scope reply function */
+            var reply = scope.replyFn();
 
-        scope.replyFunction = function() {
-          reply(scope.messageid, scope.content);
+            scope.replyFunction = function() {
+A              reply(scope.messageid, scope.content);
+            };
+
+            scope.setUnread = function(message) {
+              MessageService.setUnread(message.id);
+            };
+
+            scope.setFollowUp = function(message){
+              MessageService.setFollowUp(message.id);
+            };
+
+            scope.delMessage = function(message){
+              MessageService.deleteMessage(message.id);
+              $state.go("messages");
+            };
+
+          }
         };
-
-        scope.setUnread = function(message) {
-          scope.$parent.setUnread(message);
-        };
-
-        scope.setFollowUp = function(message){
-          scope.$parent.setFollowUp(message);
-        };
-
-        scope.delMessage = function(message){
-          scope.$parent.delMessage(message);
-        };
-
       }
-    };
-  });
+    ]
+  );
 });
